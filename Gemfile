399afa02cdaa89e1be82a76441c87f25d30ed14c
Puru Dahal <pdahal@me.com>
2014-12-13 17:20:53 -0800

    Initial skeleton setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+
+gem 'bootstrap-sass', require: false
+gem 'middleman', '~>3.3.7'
+
+# Live-reloading plugin
+gem 'middleman-livereload', '~> 3.1.0'
+
+# Assets
+gem 'middleman-sprockets'
---
+++ config.rb
@@ -0,0 +1,52 @@
+###
+#Bootstrap sass
+activate :sprockets
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+activate :livereload, :host => "colorize.dev"
+
+configure :development do
+  activate :livereload
+end
+
+# A path which all have the same layout
+with_layout :shared_layout do
+end
+
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+# Directory
+activate :directory_indexes
