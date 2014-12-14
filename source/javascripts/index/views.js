00520399a837002076187487b34c119a0ba6c9ad
Puru Dahal <pdahal@me.com>
2014-12-13 20:49:12 -0800

    Change close button to picked color

--- source/javascripts/index/views.js
+++ source/javascripts/index/views.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
   var client = new ZeroClipboard( $('.click-to-copy') );
 
-  var messages = ["Good Job", "You Suck", "Holy Shit", "Oh Fuck", "No Way", "Dammn.."]
+  var messages = ["Good Job", "You Suck", "Holy Shit", "Oh Fuck", "Hell Yeah", "Dammn.."]
 
   client.on( "ready", function( readyEvent ) {
     client.on( "aftercopy", function( event ) {
-      swal(messages[Math.floor(Math.random()*messages.length)]+"!", "You just copied "+ event.data['text/plain'], "success")
-    } );
-  } );
+      var color = event.data['text/plain']
 
+      swal({
+        title: messages[Math.floor(Math.random()*messages.length)]+"!",
+        text: "Go forth and design with "+ color,
+        type: "success",
+        confirmButtonText: "Cool",
+        confirmButtonColor:  color,
+        allowOutsideClick: true
+      });
+    });
+  });
 
   client.on( 'error', function(event) {
     ZeroClipboard.destroy();
   } );
-
-
 });
